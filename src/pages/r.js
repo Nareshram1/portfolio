a1bfa8754b0aedc8cb34aa5f16ee9cf64f09a663
Nareshram1.Naresh <Nareshram1.nireshram805s1@gmail.com>
2024-01-14 21:29:10 +0530

    commented getserversideprop as its next 12

--- src/pages/r.js
+++ src/pages/r.js
@@ -1,18 +1,18 @@
  const r=({name})=>{
     return(
         <>
-        <h3 className="flex align-middle">{name}</h3>
+        <h3 className="flex align-middle">Naresh</h3>
         </>
     )
  }
 
  export default r;
+// NEXT 12
+//  export async function getServerSideProps(e){
+//     //  const data =await import("URL")
 
- export async function getServerSideProps(e){
-    //  const data =await import("URL")
-
-     return {
-        props:{'name':"Naresh Ram B"}
+//      return {
+//         props:{'name':"Naresh Ram B"}
     
-    }
- }
+//     }
+//  }
