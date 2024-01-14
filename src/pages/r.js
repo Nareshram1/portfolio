282af507c7bc3c824535f94368c7fd2f0bd147bf
Nareshram1.Naresh <Nareshram1.nireshram805s1@gmail.com>
2024-01-14 16:08:14 +0530

    testing changes

---
+++ src/pages/r.js
@@ -0,0 +1,18 @@
+ const r=({name})=>{
+    return(
+        <>
+        <h3 className="flex align-middle">{name}</h3>
+        </>
+    )
+ }
+
+ export default r;
+
+ export async function getServerSideProps(e){
+    //  const data =await import("URL")
+
+     return {
+        props:{'name':"Naresh"}
+    
+    }
+ }
